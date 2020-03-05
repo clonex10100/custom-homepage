7c1eba39ed6a3e72881785edb855315942568c9b
clonex10100 <clonex10100@gmail.com>
2020-03-05 17:49:26 -0500

    added basic seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+homepages = ["entertainment", "work", "social"].map{ |name| Homepage.create(name: name) }
+
+homepages[0].bookmark_modules.build(name: 'streaming');
+homepages[0].bookmark_modules.build(name: 'blogs');
+
+homepages[1].bookmark_modules.build(name: 'docs');
+homepages[1].bookmark_modules.build(name: 'wikis');
+
+homepages[2].bookmark_modules.build(name: 'forums');
+homepages[2].bookmark_modules.build(name: 'email');
